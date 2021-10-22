bf4f6b21bd0370a01f95005cf8bcb1f5f7d251a3
serg155alternate <serg1557733@i.ua>
2021-10-22 19:02:23 +0300

    add first section

--- src/App.js
+++ src/App.js
@@ -2,62 +2,67 @@
 import { Component, StrictMode} from 'react';  //  destruct sintax
 import './App.css';
 
-
-const Header = () => {
-  return <h2>Hello world</h2>
-}
-
-
-/* const Field = () => {
-  const holder = 'Enter here';
-  const styleField = {
-    width: '300px'
-  }
-  return <input     
-            placeholder= {holder} 
-            type="text" 
-            style = {styleField}
-         />
-} */
-
-
-//using another sintax
-class Field extends Component {
+//using another sintax class
+class Nav extends Component {
     render() {
-        const holder = 'Enter here';
-        const styleField = {
-            width: '300px'
-            };
-        return <input     
-        placeholder= {holder} 
-        type="text" 
-        style = {styleField}
-     />
+        return  <nav class="header__nav"> 
+                    <ul>
+                        <li class="header__link login"><a href="#">LOG IN</a></li>
+                        <li class="header__link sign"><a href="#">SIGN UP</a></li>
+                    </ul> 
+                    <div class="hamburger">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>   
+                </nav> 
         }
      
 }
 
+
+//another function 
+
 function Btn() {
 
   const text = 'Log in';
-  const logged = true;
- /*  const result = () => {
-     return <p>Log in</p>
-  } */
- return <button>{logged?'Enter': text}</button>
+  const logged = true; //conditions
+
+ return <div class="btn btn_header">
+            <a href="#"> {logged?'GET STARTED': text}</a>
+        </div>
 }
 
-function App() {
-  return (
-    <div className="App">
-        <StrictMode>
-            <Header/>
-        </StrictMode>
-        <Field/>
-        <Btn/>
-    </div>
-  );
-}
+const Logo = () => <a href="#"><img src="logo.png" alt="Food"/></a>;
+
+//another const 
+const Header = () => {
+    return <header class="header">
+              <div class="container">
+                  <div class="header__top">
+                      <Logo/>
+                      <Nav/>
+                  </div>
+                  <div class="header__content">
+                      <h2>Find your Favorite</h2>
+                      <h1>RECIPES</h1>
+                      <Btn/>
+                      <h4>OR SEND US YOUR OWN RECIPES AND GET REWARDED!</h4>
+                  </div>
+              </div>
+          </header>
+  }
+
+  function App() {
+    return (
+      <div>
+          <StrictMode>
+              <Header/>
+          </StrictMode>
+      </div>
+    );
+  }
+
 
 export default App;
 
